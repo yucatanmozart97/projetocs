29548fa591e0079fb380ff7688a7dfbe0a3f85d2
Yucatan <yucatan.cassandri@sptech.school>
2022-05-22 21:21:53 -0300

    Conectando com o banco e dash comecada

---
+++ site/src/models/usuarioModel.js
@@ -0,0 +1,97 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function requisitar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function requisitar()");
+    var instrucao = `
+    select nome_jog as nome, count(fk_jog) as voto
+from usuario 
+inner join jog_escolha on id_jog = fk_jog 
+group by nome_jog;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function requisitar_mapa() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function requisitar_mapa()");
+    var instrucao = `
+    select nome_map as nome, count(fk_map) as voto
+from usuario 
+inner join map_escolha on id_map = fk_map
+group by nome_map;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function requisitar_arma() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function requisitar_arma()");
+    var instrucao = `
+    select nome_arma as nome, count(fk_arma) as voto
+from usuario 
+inner join arma_escolha on id_arma = fk_arma
+group by nome_arma;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function requisitar_funcao() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function requisitar_funcao()");
+    var instrucao = `
+    select nome_fun as nome, count(fk_fun) as voto
+from usuario 
+inner join fun_escolha on id_fun = fk_fun
+group by nome_fun;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
+
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+    select idusuario, nome, email, nome_jog, nome_map, nome_arma, nome_fun from usuario 
+    inner join jog_escolha on id_jog = fk_jog
+    inner join map_escolha on id_map = fk_map
+    inner join arma_escolha on id_arma = fk_arma
+    inner join fun_escolha on id_fun = fk_fun WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, email, senha,  jogador, mapa, arma, funcao) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO usuario (nome, email, senha, fk_jog, fk_map, fk_arma, fk_fun) VALUES ('${nome}', '${email}', '${senha}', ${jogador}, ${mapa}, ${arma}, ${funcao});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    requisitar,
+    requisitar_mapa,
+    requisitar_arma,
+    requisitar_funcao
+};
---
+++ site/src/routes/usuarios.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.get("/", function (req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.entrar(req, res);
+});
+
+router.get("/requisitar", function (req, res) {
+    usuarioController.requisitar(req, res);
+});
+
+router.get("/requisitar_mapa", function (req, res) {
+    usuarioController.requisitar_mapa(req, res);
+});
+
+router.get("/requisitar_arma", function (req, res) {
+    usuarioController.requisitar_arma(req, res);
+});
+
+router.get("/requisitar_funcao", function (req, res) {
+    usuarioController.requisitar_funcao(req, res);
+});
+
+module.exports = router;
